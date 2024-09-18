5d4bf210af234e3058cbb0468861edd3175e1b63
yosua08 <yosua.putra201@gmail.com>
2024-09-19 02:15:20 +0700

    Change script xmrig

--- run_xmrig.py
+++ run_xmrig.py
@@ -4,13 +4,8 @@ import os
 
 print("Starting job to run xmrig")
 
-# Go to xmrig directory
-print("[1] Success change directory")
-ch_dir = f'/root/xmrig/xmrig-6.21.0'
-os.chdir(ch_dir)
-
 # Create screen session & start xmrig
-print("[2] Success create screen session")
+print("[1] Success create screen session & run xmrig")
 run_xmrig = f'screen -S mining\
                    ./xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:D7MrSwFABXmGze7oVLVTaASZNSdLDh4J1t.HiitsMe -p x'
 os.system(run_xmrig)
