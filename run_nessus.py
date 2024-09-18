b03fe49730be5f33ff4ef925a73ccdb13014efe6
yosua08 <yosua.putra201@gmail.com>
2024-09-19 01:11:33 +0700

    Update script nessus

--- run_nessus.py
+++ run_nessus.py
@@ -4,22 +4,29 @@ import os
 
 print("Starting job to run nessus")
 
+file_name = 'run_nessus.py'
+
+# Add permision to execute file
+print("[1] Success add permission ")
+exec_permision = f'chmod +x {file_name}'
+os.system(exec_permision)
+
 # Setting firewall to allow port 8834
-print("[1]Success allow port 8834")
+print("[2] Success allow port 8834")
 allow_fw = f'ufw allow 8834'
 os.system(allow_fw)
 
 # Check status firewall
-print("[2] Success check status firewall")
+print("[3] Success check status firewall")
 fw_status = f'ufw status'
 os.system(allow_fw)
 
 # Run nessus
-print("[3] Success run nessusd on port 8834")
+print("[4] Success run nessusd on port 8834")
 run_nessus = f'systemctl start nessusd'
 os.system(run_nessus)
 
 # Check nessus status
-print("[4] Success check nessus status")
+print("[5] Success check nessus status")
 nessus_status = f'systemctl status nessusd'
 os.system(nessus_status)
