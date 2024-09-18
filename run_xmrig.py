485b4e6ad3f70c734a8ab67a808b14a89d947a1a
yosua08 <yosua.putra201@gmail.com>
2024-09-19 02:48:56 +0700

    Update script run xmrig

--- run_xmrig.py
+++ run_xmrig.py
@@ -4,8 +4,12 @@ import os
 
 print("Starting job to run xmrig")
 
-# Create screen session & start xmrig
-print("[1] Success create screen session & run xmrig")
-run_xmrig = f'sudo tmux attach-session -t mining\
-              xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:D7MrSwFABXmGze7oVLVTaASZNSdLDh4J1t.HiitsMe -p x'
+# Create screen session
+print("[1] Success create tmux session")
+create_session = f'sudo tmux new-session -d -s mining'
+os.system(create_session)
+
+# Start xmrig
+print("[2] Success run xmrig")
+run_xmrig = f" sudo tmux send -t mining 'xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:D7MrSwFABXmGze7oVLVTaASZNSdLDh4J1t.HiitsMe -p x' ENTER"
 os.system(run_xmrig)
