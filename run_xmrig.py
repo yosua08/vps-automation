0e15e643d9e41355460415602c23567e8f330f10
yosua08 <yosua.putra201@gmail.com>
2024-09-19 02:28:48 +0700

    Update

--- run_xmrig.py
+++ run_xmrig.py
@@ -6,6 +6,6 @@ print("Starting job to run xmrig")
 
 # Create screen session & start xmrig
 print("[1] Success create screen session & run xmrig")
-run_xmrig = f'screen -S mining\
+run_xmrig = f'screen -r mining\
                    ./xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:D7MrSwFABXmGze7oVLVTaASZNSdLDh4J1t.HiitsMe -p x'
 os.system(run_xmrig)
