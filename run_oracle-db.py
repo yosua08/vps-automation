23c0a1f566d81885fdce58940dca4fbed60ab9b7
yosua08 <yosua.putra201@gmail.com>
2024-10-08 21:37:50 +0700

    Update

--- run_oracle-db.py
+++ run_oracle-db.py
@@ -5,7 +5,7 @@ import argparse
 
 print("Starting job to run oracle db")
 
-def start():
+def run():
     # Allow port 1521
     print("[1] Success allow port 1521")
     allow_fw = f'sudo ufw allow 1521'
@@ -13,10 +13,21 @@ def start():
 
     # Start oracle db
     print("[2] Success run oracle db on port 1521")
-    start_command = f'sudo docker run -d --name oracle-xe \
+    run_command = f'sudo docker run -d --name oracle-xe \
                     -p 1521:1521 -p 5500:5500 \
                     -e ORACLE_PWD=G@ring123 \
                     container-registry.oracle.com/database/express:latest'
+    os.system(run_command)
+
+def start():
+    # Allow port 1521
+    print("[1] Success allow port 1521")
+    allow_fw = f'sudo ufw allow 1521'
+    os.system(allow_fw)
+
+    # Start oracle db
+    print("[2] Success start oracle db on port 1521")
+    start_command = f'sudo docker start oracle-xe'
     os.system(start_command)
 
 def stop():
@@ -30,13 +41,24 @@ def stop():
     deny_fw = f'sudo ufw deny 1521'
     os.system(deny_fw)
 
+def remove():
+    # Remove existing container 
+    print("[1] Success remove existing oracle db process")
+    stop_command = f'sudo docker rm oracle-xe'
+    os.system(stop_command)
+
+    # Close port 1521
+    print("[2] Success close port 1521")
+    deny_fw = f'sudo ufw deny 1521'
+    os.system(deny_fw)
+
 def main():
     parser = argparse.ArgumentParser(description="Control the process with start and stop options.")
 
     parser.add_argument(
         'command',
-        choices=['start', 'stop'],
-        help="Choose what action to perform: start, stop and check-session."
+        choices=['start', 'stop', 'run', 'remove'],
+        help="Choose what action to perform: start, stop, run and remove."
     )
 
     args = parser.parse_args()
@@ -45,6 +67,10 @@ def main():
         start()
     elif args.command == 'stop':
         stop()
+    elif args.command == 'run':
+        run()
+    elif args.command == 'remove':
+        remove()
 
 if __name__ == '__main__':
     main()
